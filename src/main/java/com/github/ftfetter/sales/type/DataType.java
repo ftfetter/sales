46072ec5b50739a48d1770ca98a3f3bd574496ef
felipe.fetter <ilegra.felipef@agibank.com.br>
2018-10-22 10:35:11 -0300

    Adding type by id

--- src/main/java/com/github/ftfetter/sales/type/DataType.java
+++ src/main/java/com/github/ftfetter/sales/type/DataType.java
@@ -1,9 +1,22 @@
 package com.github.ftfetter.sales.type;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum DataType {
 
-    CUSTOMER,
-    SALES,
-    SALESMAN;
+    SALESMAN    ("001"),
+    CUSTOMER    ("002"),
+    SALES       ("003");
 
+    private String id;
+
+    DataType(String id) {
+    }
+
+    public static Optional<DataType> getById(String id) {
+        return Arrays.stream(DataType.values())
+                .filter(dataType -> dataType.id.equals(id))
+                .findFirst();
+    }
 }
