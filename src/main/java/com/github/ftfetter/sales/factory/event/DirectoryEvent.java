28185578f0950f2addbfbf578731b345d5cdaf3e
felipe.fetter <ilegra.felipef@agibank.com.br>
2018-10-25 11:39:49 -0300

    Changing application structure

--- src/main/java/com/github/ftfetter/sales/factory/event/DirectoryEvent.java
+++
@@ -1,9 +0,0 @@
-package com.github.ftfetter.sales.factory.event;
-
-import java.nio.file.WatchEvent;
-
-public interface DirectoryEvent {
-
-    Boolean isElegible(WatchEvent event);
-    void execute(String fileName);
-}
